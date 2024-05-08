86a3eef1ff41b930b7c66bada93eb6c7b0029260
kai-lee1 <66237421+kai-lee1@users.noreply.github.com>
2024-05-08 15:15:32 -0700

    stuff

--- player.py
+++ player.py
@@ -1,1 +1,15 @@
+import numpy as np
+from board import Board
 #hand, score. we can add the destination tickets later
+class Player:
+    def __init__(self, board: Board):
+        """
+        Initializes a new instance of the Player class.
+        """
+        self.board: Board = board
+        self.hand: np.ndarray = np.array([])
+        self.score: np.ndarray = np.array([])
+    
+    def draw_card ():
+        pass
+    
